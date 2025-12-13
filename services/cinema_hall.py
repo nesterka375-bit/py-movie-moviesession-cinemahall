1224ecd0a6ce725a914ba40487ed1f8825b40463
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-12-13 12:19:14 +0100

    solved py-movie-moviesession-cinemahall

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -2,7 +2,7 @@ from django.db.models import QuerySet
 from db.models import CinemaHall
 
 
-def get_cinema_hall() -> QuerySet:
+def get_cinema_halls() -> QuerySet:
     return CinemaHall.objects.all()
 
 
