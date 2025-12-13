d87dc758d28a3cce53e9c3f3d8cbcf57d3313891
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-12-13 12:16:01 +0100

    solved py-movie-moviesession-cinemahall

--- db/models.py
+++ db/models.py
@@ -45,7 +45,6 @@ class MovieSession(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return (
-            f"{self.cinema_hall.name}"
-            f" {self.show_time.strftime("%Y-%m-%d%H:%M:%S")}"
-        )
+        name = self.cinema_hall.name
+        time = self.show_time.strftime("%Y-%m-%d%H:%M:%S")
+        return f"{name} {time}"
