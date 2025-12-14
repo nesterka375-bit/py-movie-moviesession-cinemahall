9e77b53e79094a0975d5f82467e37445519dfaab
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-12-14 16:41:06 +0100

    solved py-movie-moviesession-cinemahall

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,8 +5,8 @@ from contextlib import redirect_stdout
 from io import StringIO
 
 from db.models import Actor, Genre, Movie, MovieSession, CinemaHall
-from db.services import get_movies, get_movie_by_id, create_movie
-from db.services import get_cinema_halls, create_cinema_hall
+from db.services.movie import get_movies, get_movie_by_id, create_movie
+from db.services.cinema_hall import get_cinema_halls, create_cinema_hall
 from db.services import (
     create_movie_session,
     get_movies_sessions,
